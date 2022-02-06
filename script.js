6fe707fabcdcfd3017412311c0f7aa03fb12487b
MuhammadAbdiel <abdielfirdie@gmail.com>
2022-02-06 14:50:51 +0700

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let nasiGoreng = document.querySelector("#nasi-goreng");
+nasiGoreng.setAttribute("src", "assets/image/nasi-goreng2.jpg");
+
+nasiGoreng.addEventListener("click", function (event) {
+  document.querySelector("#nasi-goreng");
+  nasiGoreng.setAttribute("src", "assets/image/nasi-goreng.jpg");
+});

db01a678a2010d018f74a66de65887d9ea98a9d3
Muhammad Abdiel Firjatullah <abdielfirdie@gmail.com>
2022-05-28 15:48:19 +0700

    Add dropdown menu when website opened in mobile device or device that have screen size max: 576px

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-let nasiGoreng = document.querySelector("#nasi-goreng");
-nasiGoreng.setAttribute("src", "assets/image/nasi-goreng2.jpg");
+const nasiGoreng = document.querySelector("#nasi-goreng");
 
-nasiGoreng.addEventListener("click", function (event) {
-  document.querySelector("#nasi-goreng");
+nasiGoreng.addEventListener("mouseover", function () {
+  nasiGoreng.setAttribute("src", "assets/image/nasi-goreng2.jpg");
+});
+
+nasiGoreng.addEventListener("mouseout", function () {
   nasiGoreng.setAttribute("src", "assets/image/nasi-goreng.jpg");
+});
+
+const menu = document.querySelector(".menu");
+const dropdown = document.querySelector("nav div");
+
+document.addEventListener("click", function (e) {
+  if (e.target.className !== "menu") {
+    dropdown.classList.remove("dropdown");
+    dropdown.style.display = "none";
+    menu.style.fontWeight = "400";
+  } else {
+    dropdown.classList.toggle("dropdown");
+    if (dropdown.classList.contains("dropdown")) {
+      dropdown.style.display = "flex";
+      menu.style.fontWeight = "bold";
+    } else {
+      dropdown.style.display = "none";
+      menu.style.fontWeight = "400";
+    }
+  }
 });
